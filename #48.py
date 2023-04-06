59a7049ffe9963b3b66923fceca45f380246595d
reyllama <rin4616@gmail.com>
2023-04-06 11:36:07 +0900

    Rotate Image

---
+++ #48.py
@@ -0,0 +1,24 @@
+"""
+2023.04.06
+"""
+"""
+48. Rotate Image
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+"""
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # elements that are altered by row <- column operation (that affects later operations)
+        cache = [[] for _ in range(n)]
+        # Replace each row with corresponding column, but chunked to consider prior replacements
+        for r in range(n):
+            for c in range(r+1, n):
+                cache[c].append(matrix[r][c])
+            # Concatenate column chunk and cache chunk
+            matrix[r] = [matrix[n-k-1][r] for k in range(n)][:n-r] + cache[r][::-1]
---
+++ #49.py
@@ -0,0 +1,13 @@
+"""
+2023.04.06
+"""
+"""
+49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
