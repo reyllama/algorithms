6fff20366c5cebf391dd188233408d124a17102f
reyllama <rin4616@gmail.com>
2023-04-06 11:46:55 +0900

    Group Anagrams

--- #49.py
+++ #49.py
@@ -10,4 +10,14 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 """
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = []
+        from collections import defaultdict
+        cache = defaultdict(list)
+        for st in strs:
+            sst = "".join(sorted(st))
+            cache[sst].append(st)
+        return [v for v in cache.values()]
+
+"""
+Time > 87.48%, Memory > 82.72%
+Sort once, and use this sorted version as the key for each anagram group.
+"""
